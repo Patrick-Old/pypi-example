cfa1a880d0eca930f2f235ffce97993d0c5472d0
Patrick Old <patold@uw.edu>
2020-10-24 16:13:31 -0700

    fixed setup.py bug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='patrick-helloworld',
-    version='0.0.2',
+    version='0.0.1',
     description='Say Hello!',
     py_modules=['helloworld'],
     package_dir={'': 'src'},
